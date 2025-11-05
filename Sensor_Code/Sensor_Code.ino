ac2450646dce1767259d418afdbf80cdc4796630
Kaylie <kaylierp06@gmail.com>
2025-11-05 13:33:29 -0600

    Sensor-LED Code
    Code for simple sensor & LED interaction

---
+++ Sensor_Code/Sensor_Code.ino
@@ -0,0 +1,24 @@
+
+const int sensorPin = A0;
+const int ledPin = 13;
+int sensorVal;
+
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  sensorVal = analogRead(sensorPin);
+  Serial.println(sensorVal);
+  if(sensorVal >= 250){
+    digitalWrite(ledPin, HIGH);
+  } else{
+    digitalWrite(ledPin, LOW);
+  }
+
+  delay(500);
+  // put your main code here, to run repeatedly:
+}
