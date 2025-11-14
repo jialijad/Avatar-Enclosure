cd41a1556cdd16b072a760be8539189e915edafa
Kaylie <kaylierp06@gmail.com>
2025-11-14 14:20:09 -0600

    Update Avatar_Code.ino
    adding more servos

--- Avatar_Code/Avatar_Code.ino
+++ Avatar_Code/Avatar_Code.ino
@@ -1,7 +1,9 @@
 #include <Servo.h>
 
-Servo bendersServo;
-Servo rokuServo;
+Servo benderServo1;
+Servo benderServo2;
+Servo rokuServo; //variable for roku's servo
+Servo aangServo; //variable for aang's servo
 const int mapLeds = 13;
 const int rokuSwitch = 10;
 int i= 0;
@@ -9,13 +11,13 @@ int angle = 0;
 
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(mapLeds, OUTPUT);
-  bendersServo.attach(9);
+  bendersServo1.attach(9);
   Serial.begin(9600);
-  rokuServo.attach(8);
+  rokuServo.attach(8); //pin for roku servo
+  aangServo.attach(6); //pin for aang's servo
   pinMode(rokuSwitch, INPUT);
-  rokuServo.write(0);
+  rokuServo.write(0); //initial state for roku
 }
 
 void servoSlow(Servo &servo, int start, int end, int delayt){
@@ -28,20 +30,19 @@ int step;
   }
   for (int a = start; a != end; a += step) {
     servo.write(a);
-    delay(delayt);    // increase this to make it slower
+    delay(delayt);    //delayt value controls speed
    }
   servo.write(end);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   digitalWrite(mapLeds, HIGH);
   if(angle == 0){
-   servoSlow(bendersServo, angle, 180, 20);
+   servoSlow(bendersServo1, angle, 180, 20);
    angle = 180;
   }
   if (angle == 180){
-   servoSlow(bendersServo, 180, 0, 20);
+   servoSlow(bendersServo1, 180, 0, 20);
    angle = 0;
   }
 
@@ -51,4 +52,6 @@ void loop() {
   else{
   rokuServo.write(0);
   }
+
+
 }
