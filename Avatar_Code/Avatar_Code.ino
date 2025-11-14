87c5394438929d5cdaea07c472d818d52550178d
Kaylie <kaylierp06@gmail.com>
2025-11-14 12:38:48 -0600

    Update Avatar_Code.ino

--- Avatar_Code/Avatar_Code.ino
+++ Avatar_Code/Avatar_Code.ino
@@ -7,36 +7,35 @@ int angle = 0;
 
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(mapLeds, OUTPUT);
   bendersServo.attach(9);
   Serial.begin(9600);
 }
 
-void servoSlow(int start, int end){
-  int step;
-  if (endAngle > startAngle) {
-    step = 1;
-  } else {
-    step = -1;
-  }
-  for (int a = start; a != end; a += step) {
-    bendersServo.write(a);
-    delay(15);
-  }
-  bendersServo.write(end);
+void servoSlow(Servo &servo, int start, int end, int delayt){
+int step;
+
+if (end > start) {
+  step = 1;
+} else {
+  step = -1;
+}
+for (int a = start; a != end; a += step) {
+    servo.write(a);
+    delay(delayt);
+ }
+servo.write(end);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   digitalWrite(mapLeds, HIGH);
 if(angle == 0){
-  bendersServo.write(angle);
-  angle = 90;
+  servoSlow(bendersServo, angle, 180, 20);
+  angle = 180;
 
 }
-if (angle == 90){
-  bendersServo.write(angle);
+if (angle == 180){
+  servoSlow(bendersServo, 180, 0, 20);
   angle = 0;
 }
 }
