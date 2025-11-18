b8b76617160f93bdf3135376ad90afcd61c5775d
Kaylie <kaylierp06@gmail.com>
2025-11-17 20:43:52 -0600

    Update Avatar_Code.ino
    new back & forth loop

--- Avatar_Code/Avatar_Code.ino
+++ Avatar_Code/Avatar_Code.ino
@@ -1,53 +1,60 @@
 #include <Servo.h>
 
 Servo benderServo1;
-Servo benderServo2;
+int bendersAngle = 41;
+int benderSpeed = 0.5;
 Servo rokuServo; //variable for roku's servo
 Servo aangServo; //variable for aang's servo
 const int mapLeds = 13;
-const int rokuSwitch = 10;
+const int rokuSwitch = 8;
 const int iceSwitch = 7;
 const int fireLED = 5; //fire nation led
 int i= 0;
-int angle = 0;
-
+int turn = 0;
 
 void setup() {
   pinMode(mapLeds, OUTPUT);
   pinMode(fireLED, OUTPUT);
 
-  pinMode(rokuSwitch, INPUT); //initializes the switch to make roku dissappear
-  pinMode(iceSwitch, INPUT); //initializes the switch to make aang appear
+  pinMode(rokuSwitch, INPUT); //switch to make roku dissappear
+  pinMode(iceSwitch, INPUT); //switch to make aang appear
 
   benderServo1.attach(9);
-  benderServo2.attach(10);
+  benderServo1.write(0);
 
   Serial.begin(9600);
 
-  rokuServo.attach(8); //pin for roku servo
+  rokuServo.attach(10); //pin for roku servo
   aangServo.attach(6); //pin for aang's servo
   rokuServo.write(0); //initial state for roku
   aangServo.write(0); //initial state for aang
+  Serial.begin(9600);
 }
 
 void loop() {
   digitalWrite(mapLeds, HIGH);
-  if(angle == 0){
-   //servoSlow(benderServo1, angle, 180, 20);
-  // servoSlow(benderServo2, 180, angle, 20);
-   angle = 180;
+  if(angle == 1){
+      benderServo1.write(0);   // spin CCW
+     angle = 0;
   }
-  if (angle == 180){
-   //servoSlow(benderServo1, 180, 0, 20);
-  // servoSlow(benderServo2, 0, 180, 20);
-   angle = 0;
+
+  else if (angle == 0){
+      benderServo1.write(180);  // spin CW
+     angle = 1;
   }
 
   if(digitalRead(rokuSwitch)== HIGH){
- // servoSlow(rokuServo, 0, 180, 20);
+    Serial.println("byeroku");
   }
   else{
-  rokuServo.write(0);
+    rokuServo.write(0);
+    Serial.println("hiroku");
+  }
+
+  if(digitalRead(iceSwitch)== HIGH){
+  }
+  else{
+    aangServo.write(0);
   }
 
 
